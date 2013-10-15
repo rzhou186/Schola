8ae1ff38631e489a6cc8a15e22b636e7ba0f8caf
Ray Zhou <rzhou186@stanford.edu>
2013-10-15 02:36:09 -0700

    Added hacky functions for appending new posts and requests to the DOM

--- public/javascripts/postStream.js
+++ public/javascripts/postStream.js
@@ -2,15 +2,55 @@ $(document).ready(function() {
 
   data = {};
   data["start"] = 0;
+
+  loadMorePosts(data);
+
+  socket.on('getPostsSuccess', function(posts) {
+    data["start"] += 10;  // Don't hardcode magic numbers...
+    appendPosts(posts["result"]);
+    $(".postStreamLoading").fadeOut("fast");
+  });
+
+  // When the user scrolls to the bottom, call loadMorePosts
+
+});
+
+function loadMorePosts(data) {
   $(".postStreamLoading").show();
   socket.emit('getPosts', data);
+}
 
-});
+function appendPosts(posts) {
+  for (var i=0; i<posts.length; i++) {
+    var post = posts[i];
 
-socket.on('getPostsSuccess', function(data) {
-  $(".postStreamLoading").fadeOut("fast");
-});
+    $("#posts").append(
+      "<div class=\"post\">" + 
+        "<div class=\"postSide\">" + 
+          "<div class=\"postTitle\">" + 
+            "<span class=\"postUsername\">" + "testingdude" + " </span>" + 
+            "added a post." + 
+          "</div>" + 
+          "<div class=\"postDate\">" + "22 Apr" + "</div>" + 
+          "<div class=\"postViews\">" + post["views"] + 
+            " <span class=\"glyphicon glyphicon-eye-open\"></span>" + 
+          "</div>" + 
+        "</div>" +
+        "<div class=\"postMain\">" + 
+          "<div class=\"postIcon\">" + 
+            "<span class=\"glyphicon glyphicon-link\"></span>" + 
+          "</div>" + 
+          "<div class=\"postName\"><a href=\"" + post["URL"] + "\">" + post["name"] + "</a>" + 
+          "</div>" + 
+          "<div class=\"postDescription\">" + 
+            "<p>" + post["desc"] + "</p>" + 
+          "</div>" + 
+          "<div class=\"postAccess\">" + 
+            "<button class=\"postGet btn btn-schola btn-xs\">View</button>" + 
+          "</div>" + 
+        "</div>" + 
+      "</div>"
+    );
 
-function appendPosts(data) {
-
+  }
 }
--- public/javascripts/requestStream.js
+++ public/javascripts/requestStream.js
@@ -2,15 +2,50 @@ $(document).ready(function() {
 
   data = {};
   data["start"] = 0;
+
+  loadMoreRequests(data);
+
+  socket.on('getRequestsSuccess', function(requests) {
+    data["start"] += 10;  // Don't hardcode magic numbers...
+    appendRequests(requests["result"]);
+    $(".requestStreamLoading").fadeOut("fast");
+  });
+
+  // When the user scrolls to the bottom, call loadMoreRequests
+
+});
+
+function loadMoreRequests(data) {
   $(".requestStreamLoading").show();
   socket.emit('getRequests', data);
+}
 
-});
+function appendRequests(requests) {
+  for (var i=0; i<requests.length; i++) {
+    var request = requests[i];
 
-socket.on('getRequestsSuccess', function(data) {
-  $(".requestStreamLoading").fadeOut("fast");
-});
+    $("#requests").append(
+      "<div class=\"request\">" + 
+        "<div class=\"requestMain\">" + 
+          "<div class=\"requestTitle\">" + 
+            "<span class=\"requestUsername\">" + "anonymous" + " </span>requested:" + 
+          "</div>" + 
+          "<div class=\"requestDate\">" + "16 Apr" + "</div>" + 
+          "<div class=\"requestName\">" +
+            "<a href=\"" + request["URL"] + "\">" + request["name"] + "</a>" + 
+          "</div>" + 
+        "</div>" + 
+        "<div class=\"requestSide\">" + 
+          "<div class=\"requestStatus\">" +
+            "<span class=\"glyphicon glyphicon-unchecked\"></span>" + 
+          "</div>" + 
+          "<button class=\"btn btn-default btn-xs btn-block\">" + 
+            "<span class=\"glyphicon glyphicon-chevron-up\"></span>" + 
+            "<div class=\"requestUpvotes\">" + request["upvotes"] + "</div>" + 
+          "</button>" + 
+        "</div>" + 
+      "</div>"
+    );
 
-function appendRequests(data) {
-
+  }
 }
