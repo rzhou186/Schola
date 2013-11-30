bdfec04fdf8e5928373352f781525e73a8c2677b
Ray Zhou <rzhou186@stanford.edu>
2013-11-29 19:04:00 -0800

    Removed some test debug statements

--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -1,8 +1,2 @@
 var socket;
-
 socket = io.connect('/');
-socket.emit('test', {success:true});
-socket.on('testCallback', function(data) {
-	console.log('client side test successful');
-});
-console.log(CryptoJS.SHA1("Message").toString(CryptoJS.enc.hex));
